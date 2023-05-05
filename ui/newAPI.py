8977d031c7e1754872fd7c87986e359c5770f3e6
hafizh.hm81@gmail.com <hafizh.hm81@gmail.com>
2023-05-05 21:51:30 +0700

    fix port

--- ui/newAPI.py
+++ ui/newAPI.py
@@ -8,7 +8,7 @@ api = Api(app)
 
 class HitRasa(Resource):
     def post(self,message):
-        response = requests.post('https://acda-103-125-37-137.ngrok-free.app/webhooks/rest/webhook', json={"message":message})
+        response = requests.post('https://5a84-103-125-37-137.ngrok-free.app/webhooks/rest/webhook', json={"message":message})
         response_final =response.json()[0]['text']
         return {"bot": response_final}
 
@@ -33,4 +33,4 @@ api.add_resource(HitRasa, "/hitrasa/<string:message>")
     
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8069')
-    app.run(debug=True, port=server_port, host='0.0.0.0')
+    app.run(debug=True,port=server_port)
