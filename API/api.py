862d6b8338dd4df09ed1cd3a94a90e8a6dc0fd06
muyassarhafizh <hafizh.hm81@gmail.com>
2023-06-14 07:56:08 +0000

    Added the custom actions container

--- API/api.py
+++ API/api.py
@@ -1,18 +1,37 @@
 import requests
 import os
+import logging
+import time
 from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+# Configure the logging settings
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler('/app/logAPI/app.log'),  # Log to a file
+        logging.StreamHandler()  # Log to the console
+    ]
+)
+
 class HitRasa(Resource):
     def post(self):
+        start_time = time.time()
         json_data = request.get_json()
         message = json_data.get('message')
-        response = requests.post('http://rasa-app:5005/webhooks/rest/webhook', json={"message":message})
+        sender = json_data.get('sender')
+        response = requests.post('http://rasa-app:5005/webhooks/rest/webhook', json={"message":message, "sender":sender})
         response_final =response.json()[0]['text']
+
+        end_time = time.time()
+        response_time = end_time - start_time
+        logging.info('Hafizh testing : the message took {} seconds to pass'.format(response_time))
         return {"bot": response_final}
+
 
 api.add_resource(HitRasa, "/hitrasa")
     
